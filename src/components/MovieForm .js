c0004fc72bcbae6bb876fb8e81f6fc4b7417eb83
jinnyRyu <hajinryu51@gmail.com>
2022-02-13 17:44:07 +0900

    validation 오류 수정

--- src/components/MovieForm .js
+++ src/components/MovieForm .js
@@ -38,10 +38,9 @@ const MovieForm = ({ addMovie }) => {
                 year: movieYear
             });
             resetErrors();
-        } else {
+            resetForm();
+        } 
 
-        }
-        resetForm();
     };
     return (
         <form onSubmit={onSubmit}>
